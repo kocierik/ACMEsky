b53c8792d86725bd4a19caeedb055196389c8063
kocierik <kocierik@gmail.com>
2023-12-26 11:28:43 +0100

    chore: use env

--- src/airlineService/db/db.go
+++ src/airlineService/db/db.go
@@ -1,15 +1,25 @@
 package db
 
 import (
-	"log"
-
+	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/kocierik/ACMEsky/airlineService/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
+	"os"
 )
 
 func Init() *gorm.DB {
-	dbURL := "postgres://pg:pass@db:5432/airline"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	USER := os.Getenv("POSTGRES_USER")
+	PSW := os.Getenv("POSTGRES_PASSWORD")
+	DB := os.Getenv("POSTGRES_DB")
+	dbURL := fmt.Sprintf("postgres://%s:%s@db:5432/%s", USER, PSW, DB)
 
 	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
 
--- src/airlineService/main.go
+++ src/airlineService/main.go
@@ -13,12 +13,12 @@ func main() {
 
 	router := gin.Default()
 
-	router.GET("/getTickets", h.GetTicketByID)
+	router.GET("/getTicket", h.GetTicketByID)
 	router.GET("/getFlights", h.GetFlights)
 
 	log.Println("API is running!")
 	log.Println("EXAMPLE REQUEST: ")
 	log.Println("http://localhost:8080/getFlights?departure=AAA&arrival=BBB&departureDate=2023-12-25")
-	log.Println("http://localhost:8080/getTickets?id=1")
+	log.Println("http://localhost:8080/getTicket?id=1")
 	router.Run(":8080")
 }
