9550b365311f0c5d259ee9099b97ec2efbb5ca7a
ManuelArto <manuelartociao@gmail.com>
2024-07-06 19:47:34 +0200

    fix float conversion error

--- src/ACMESky/camunda/camundaworkers/workers/buy_offer/check_distance_for_transfer_booking.py
+++ src/ACMESky/camunda/camundaworkers/workers/buy_offer/check_distance_for_transfer_booking.py
@@ -13,7 +13,16 @@ def check_distance_for_transfer_booking(task: ExternalTask) -> TaskResult:
     logger = get_logger()
     logger.info("check_distance_for_transfer_booking")
 
-    if float(task.get_variable("distance")) <= 30:
+    distance: str = task.get_variable("distance")
+
+    try:
+        distance = float(distance)
+    except ValueError:
+        logger.exception(f"Invalid distance value: {distance}")
+        return task.complete(global_variables={"book_travel_company": False})
+
+    if distance <= 30:
         return task.complete(global_variables={"book_travel_company": True, "distances": json.dumps({"distances": []})})
     else:
         return task.complete(global_variables={"book_travel_company": False})
+
